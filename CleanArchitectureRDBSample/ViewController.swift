09daab4a3fbcd5feaf668df68309d97cc27232f5
菊池達也 <tatsuyakikuchi@kikuchitatsuya-no-macbook-pro.local>
2017-08-28 01:01:03 +0900

    Initial Commit

---
+++ CleanArchitectureRDBSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArchitectureRDBSample
+//
+//  Created by 菊池達也 on 2017/08/28.
+//  Copyright © 2017年 菊池達也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
